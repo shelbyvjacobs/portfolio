da0cdda905a380f3a7a03d8d737faf5212b423ac
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-24 14:00:55 -0500

    importing images to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import "./Home.css";
+import hero from "./Img/jesus-kiteque-wn-KYaHwcis-unsplash.jpg";
 
 export default function Home() {
     return (
         <div className="Home Section">
             <p className="important">Full Stack Developer.</p>
+            <img src={hero} className="hero"></img>
         </div>
     );
 };
