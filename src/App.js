7e6ffee46067573a8233d548a1151a180b94ada5
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-02 13:47:37 -0500

    added hover to nav links

--- src/App.js
+++ src/App.js
@@ -19,16 +19,12 @@ class App extends Component {
   return (
     <div className="App">
       <nav className="nav">
-        {/* <Link to="#home">Home</Link>
-        <Link to="#about">About </Link>
-        <Link to="#projects">Projects </Link>
-        <Link to="#contact">Contact </Link> */}
         <p className="nav-name">Shelby Jacobs</p>
         <div className="nav-links">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
+          <a href="#home" className="nav-link">Home</a>
+          <a href="#about" className="nav-link">About</a>
+          <a href="#projects" className="nav-link">Projects</a>
+          <a href="#contact" className="nav-link">Contact</a>
         </div>
       </nav>
       <Home />
@@ -36,35 +32,6 @@ class App extends Component {
       <Skills />
       <Projects />
       <Contact />
-      <main>
-        {/* <Switch>
-          <Route 
-            exact path="#home"
-            render={routerProps => (
-              <Home />
-            )}
-          />
-          <Route
-            exact path="#about"
-            render={routerProps => (
-              <About />
-            )}
-          />
-          <Route
-            exact path="#projects"
-            render={routerProps => (
-              <Projects />
-            )}
-          />
-          <Route
-            exact path="#contact"
-            render={routerProps => (
-              <Contact />
-            )}
-          />
-        </Switch> */}
-      </main>
-
     </div>
   );
 }
