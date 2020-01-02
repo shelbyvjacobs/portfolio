81073d59a7b943a10662507152cf855930ac6635
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-02 13:52:11 -0500

    created separate component for the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import About from "./components/About/About"
-import Home from "./components/Home/Home"
+import Nav from "./components/Nav/Nav";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
 import Contact from "./components/Contact/Contact";
-import Projects from "./components/Projects/Projects"
+import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
-// import { Route, Link, Switch } from "react-router-dom";
-
 
 class App extends Component {
   constructor(props){
@@ -18,15 +17,7 @@ class App extends Component {
   render() {
   return (
     <div className="App">
-      <nav className="nav">
-        <p className="nav-name">Shelby Jacobs</p>
-        <div className="nav-links">
-          <a href="#home" className="nav-link">Home</a>
-          <a href="#about" className="nav-link">About</a>
-          <a href="#projects" className="nav-link">Projects</a>
-          <a href="#contact" className="nav-link">Contact</a>
-        </div>
-      </nav>
+      <Nav />
       <Home />
       <About />
       <Skills />
