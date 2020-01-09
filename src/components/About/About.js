0c5b443ef1591be06431d79fa997ee6d24cc4264
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-09 14:28:40 -0500

    figuring out placement of profileimage

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,6 +8,7 @@ export default function About(props) {
                 <div className="About-img left"></div>
                 <div className="About-details-container">
                     <p className="header">An Archivist turned Developer. </p>
+                    <div className="About-profile"></div>
                     <p className="About-details">As an Archivist, I became accustomed to handling large amounts of unfiltered and unsorted information. I learned to organize and build hierarchies to ensure effective retrieval and productive workflow. I built a unique understanding of information and how it relates to the whole.</p>
                     <p className="About-details">I discovered early on that my interest laid in managing digital resources. However, I didn't know much about them beyond the theoretical, and I wanted to understand it on a deeper level. I decided to take my education into my own hands and enroll in the Software Engineering Immersive bootcamp at General Assembly.</p>
                     <p className="About-details">My experience at General Assembly taught me much more than how to code: it fueled the fire of my passion for <span className="italic">clean</span> code and accessible information. I also realized the positive impact a usuable and appealing design can have on a user's experience.</p>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,10 +6,10 @@ export default function Nav() {
         <nav className="nav">
             <p className="nav-name">Shelby Jacobs</p>
             <div className="nav-links">
-            <a href="#home" className="nav-link">Home</a>
-            <a href="#about" className="nav-link">About</a>
-            <a href="#projects" className="nav-link">Projects</a>
-            <a href="#contact" className="nav-link">Contact</a>
+                <a href="#home" className="nav-link">Home</a>
+                <a href="#about" className="nav-link">About</a>
+                <a href="#projects" className="nav-link">Projects</a>
+                <a href="#contact" className="nav-link">Contact</a>
             </div>
         </nav>
     );
