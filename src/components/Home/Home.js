aa95567ee8874d2d7b43d8dfe0cbecb92ba16b83
Shelby Jacobs <shelbyvjacobs@gmail.com>
2020-01-10 15:47:04 -0500

    added subtitle to home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ export default function Home() {
         <div className="Home" id="home">
             <div className="Home-banner">
                 <h1 className="Home-name">Shelby Jacobs</h1>
+                <h2 className="header Home-subtitle">Web Developer</h2>
             </div>
         </div>
     );
