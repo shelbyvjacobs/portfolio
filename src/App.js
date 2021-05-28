35208f2141e0e2065f41c6f0de8df4ec2d126366
Shelby <sjacobs@counterpointconsulting.com>
2021-05-28 11:46:19 -0400

    Removed old components and put up under construction message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
-import Nav from "./components/Nav/Nav";
-import About from "./components/About/About";
-import Home from "./components/Home/Home";
-import Contact from "./components/Contact/Contact";
-import Projects from "./components/Projects/Projects";
-import Skills from "./components/Skills/Skills";
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
   }
   render() {
   return (
     <div className="App">
-      <Nav />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+      <p className="header">Under Construction</p>
     </div>
   );
 }
